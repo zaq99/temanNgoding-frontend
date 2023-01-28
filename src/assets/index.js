a42ed95b15e08e1aaaf57a09ddcfb34ce4c775b4
zaq <85076901+zaq99@users.noreply.github.com>
2023-01-29 00:22:42 +0700

    fix bug

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,16 +4,6 @@ import Laptop from "./laptop.png";
 import Playing from "./playing.png";
 import Roket from "./roket.png";
 import Tangan from "./tanganChil.png";
-import LaptopKuning from "./LaptopKuning.png";
 import OwnerPhoto from "./OwnerPhoto.png";
 
-export {
-    AboutPeople,
-    Happy,
-    Laptop,
-    Playing,
-    Roket,
-    Tangan,
-    LaptopKuning,
-    OwnerPhoto,
-};
+export { AboutPeople, Happy, Laptop, Playing, Roket, Tangan, OwnerPhoto };
